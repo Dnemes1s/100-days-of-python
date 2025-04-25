5b73ae99e867f1f40b5a70ffb0c1795101d75771
Brandon <twistshadow9@gmail.com>
2025-04-25 20:55:02 +1000

    Day 1 challenge. Band name generator using inputs.

---
+++ D1.py
@@ -0,0 +1,6 @@
+print("Welcome to the band name generator program!")
+
+home_town = input("What is the name of the city you grew up in?\n")
+pet_name = input("What is the name of your pet?\n")
+band_name = home_town + " " + pet_name
+print("Your band name could be " + band_name)
