7a1cbec2d694f51becb800e1ed4dbe70bb81ed96
Brandon <twistshadow9@gmail.com>
2025-05-01 15:52:48 +1000

    D5
    Did some stuff with the range function and for loops. Made a random password generator.

---
+++ D5/D5 password generator.py
@@ -0,0 +1,48 @@
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+# Adding variables for the letters, numbers and symbols
+
+print("Welcome to the PyPassword Generator!")
+# Welcoming the user
+# Asking the user for the number of letters, symbols and numbers
+letters_count = int(input("How many letters would you like in your password?\n"))
+symbols_count = int(input("How many symbols would you like?\n"))
+numbers_count = int(input("How many numbers would you like?\n"))
+# Getting the user input for the number of letters, symbols and numbers
+
+password_list = []
+# Creating an empty list for the password
+
+for char in range(1, letters_count + 1):
+    password_list.append(random.choice(letters))
+    print(password_list)
+# Adding the letters to the password list
+
+for char in range(1, symbols_count + 1):
+    password_list.append(random.choice(symbols))
+    print(password_list)
+# Adding the symbols to the password list
+
+for char in range(1, numbers_count + 1):
+    password_list.append(random.choice(numbers))
+    print(password_list)
+# Adding the numbers to the password list
+
+random.shuffle(password_list)
+print(password_list)
+# Shuffling the password list
+
+password = ""
+# Creating an empty string for the password
+
+for char in password_list:
+    password += char
+    print(password)
+# Adding the characters to the password string
+
+print(f"Your password is: {password}")
+# Printing the password
+# This will print the password
---
+++ D5/D5 range.py
@@ -0,0 +1,6 @@
+add = 0
+
+for numbers in range(1, 101):
+    add += numbers
+print(add)
+# This will print the sum of all numbers from 1 to 100
