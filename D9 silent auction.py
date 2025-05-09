0453b57efaa72bc23b76ff108e097b5c10f89e65
Brandon <twistshadow9@gmail.com>
2025-05-09 22:40:34 +1000

    D9
    Added comments to the page

--- D9 silent auction.py
+++ D9 silent auction.py
@@ -3,18 +3,21 @@
 print("Welcome to the silent auction")
 print("We will now take the first bid")
 
-auction_state = True
-highest_bid = 0
-highest_bid_name = ""
+auction_state = True    # Sets while loop condition
+highest_bid = 0     # keeps the highest bid
+highest_bid_name = ""   # Keeps the name of the highest bidder
 
 while auction_state:
     bidder_name = str(input("Bidder name: "))
     new_bid = int(input("Bid price: "))
-    more_bidder = input("Are there more bidders ?(y/n)").lower()
-    if new_bid > highest_bid:
-        highest_bid = new_bid
-        highest_bid_name = bidder_name
-    if more_bidder == "n":
-        auction_state = False
-print(f"The highest bid was from {highest_bid_name} with a bid of${highest_bid}")
+    more_bidder = input("Are there more bidders ?(y/n)").lower()    # Checks if there are more bidders
+    
+    if new_bid > highest_bid:   # Checks if the latest bid is higher than the previous one 
+        highest_bid = new_bid   # Updates the highest
+        highest_bid_name = bidder_name  # changes the name to the highest bidder
+
+    if more_bidder == "n":  # Checks if there are more bidders
+        auction_state = False   # Sets the auction state to false to escape the while loop
+
+print(f"The highest bid was from {highest_bid_name} with a bid of${highest_bid}")   # Displays the highest bidder and the bid price
 print("Congrats on your new item")
